1c1db6df33cc78b2c0067727599b6d0b19425e1b
Carol <carolina.brandes@assurant.com>
2022-08-06 23:19:45 -0300

    delete build folder

--- build/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
-const express_jwt_1 = require("express-jwt");
-exports.auth = (0, express_jwt_1.expressjwt)({
-    secret: process.env.KEY || "",
-    algorithms: ['HS256']
-});
--- build/middlewares/bloqueio.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = (req, res, next) => {
-    const { pass } = req.query;
-    if (!pass || pass !== 'banana') {
-        return res.status(400).json('errado');
-    }
-    next();
-};
